0dce45c581445d3d3d60455d549bf171874819cf
thgdsg <thisangon03@gmail.com>
2024-01-20 21:51:04 -0300

    implementando opencv

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 from tkinter.messagebox import showerror, askyesno
 from tkinter import colorchooser
 from PIL import Image, ImageTk, ImageGrab
+import cv2 as cv
 
 WIDTH = 750
 HEIGHT = 560
@@ -15,14 +16,9 @@ def open_image():
     global file_path
     file_path = filedialog.askopenfilename(title="Open Image File", filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.gif;*.bmp")])
     if file_path:
-        global image, photo_image
-        image = Image.open(file_path)
-        new_width = int((WIDTH / 2))
-        new_height = int((HEIGHT / 2))
-        image = image.resize((new_width, new_height), Image.LANCZOS)
-
-        image = ImageTk.PhotoImage(image)
-        Imgcanvas.create_image(0, 0, anchor="nw", image=image)
+        global opencvImg
+        opencvImg = cv.imread(file_path)
+        cv.imshow("Display window", opencvImg)
 
 # function for drawing lines on the opened image
 def draw(event):
@@ -30,7 +26,7 @@ def draw(event):
     if file_path:
         x1, y1 = (event.x - pen_size), (event.y - pen_size)
         x2, y2 = (event.x + pen_size), (event.y + pen_size)
-        Drawcanvas.create_oval(x1, y1, x2, y2, fill=pen_color, outline="", width=pen_size, tags="oval")
+        canvas.create_oval(x1, y1, x2, y2, fill=pen_color, outline="", width=pen_size, tags="oval")
 
 # function for changing the pen color
 def change_color():
@@ -41,14 +37,14 @@ def change_color():
 def erase_lines():
     global file_path
     if file_path:
-        Drawcanvas.delete("oval")
+        canvas.delete("oval")
 
 # the function for saving an image
 def save_image():
     global file_path
     if file_path:
         # create a new PIL Image object from the canvas
-        image = ImageGrab.grab(bbox=(Drawcanvas.winfo_rootx(), Drawcanvas.winfo_rooty(), Drawcanvas.winfo_rootx() + Drawcanvas.winfo_width(), Drawcanvas.winfo_rooty() + Drawcanvas.winfo_height()))
+        image = ImageGrab.grab(bbox=(canvas.winfo_rootx(), canvas.winfo_rooty(), canvas.winfo_rootx() + canvas.winfo_width(), canvas.winfo_rooty() + canvas.winfo_height()))
         # open file dialog to select save location and file type
         file_path = filedialog.asksaveasfilename(defaultextension=".jpg")
         if file_path:
@@ -67,14 +63,10 @@ root.iconphoto(False, icon)
 left_frame = ttk.Frame(root, width=200, height=600)
 left_frame.pack(side="left", fill="y")
 
-# the top right canvas for displaying the image
-Imgcanvas = ttk.Canvas(root, width=(WIDTH/2), height=(HEIGHT/2))
-Imgcanvas.pack()
-
-# the bottom right canvas for displaying the drawing
-Drawcanvas = ttk.Canvas(root, width=(WIDTH/2), height=(HEIGHT/2))
-Drawcanvas.pack()
-Drawcanvas.bind("<B1-Motion>", draw)
+# the right canvas for displaying the drawing
+canvas = ttk.Canvas(root, width=WIDTH, height=HEIGHT)
+canvas.pack()
+canvas.bind("<B1-Motion>", draw)
 
 # loading the icons for the 4 buttons
 image_icon = ttk.PhotoImage(file = 'add.png').subsample(3, 3)
